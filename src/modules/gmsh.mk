9b2fe55f615242a5cb0bd6c79e5400ac57fa64dc
Til Gaertner <t.gartner@tudelft.nl>
2022-04-06 10:14:00 +0200

    fixed the LD_RUN_PATH enviroment variable this is used for linking external libraries

--- src/modules/gmsh.mk
+++ src/modules/gmsh.mk
@@ -5,4 +5,4 @@ MY_INCDIRS	+= $(GMSHDIR)/include
 MY_LIBDIRS	+= $(GMSHDIR)/lib
 MY_LIBS     += gmsh
 
-export LD_RUN_PATH = $$LD_RUN_PATH:$(GMSHDIR)/lib
+export LD_RUN_PATH := $(GMSHDIR)/lib:$(LD_RUN_PATH)
