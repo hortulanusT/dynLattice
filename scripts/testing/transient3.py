91cf95455d65783c79d2f8fe9088692a2930ee64
Til Gaertner <t.gartner@tudelft.nl>
2023-02-08 10:43:36 +0100

    added reference data for transient test 3

--- scripts/testing/transient3.py
+++ scripts/testing/transient3.py
@@ -5,17 +5,26 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# HACK get reference data imported
-
 energy = pd.read_csv("tests/transient/test3/energy.csv",
                      index_col="time")
 disp = pd.read_csv("tests/transient/test3/disp.gz", index_col="time")
+ref_disp = pd.read_csv("tests/transient/ref_data/test2_ref.csv",
+                       header=[0, 1])
+print(ref_disp)
 
 ellbow = disp[["dx[1]", "dy[1]", "dz[1]"]]
 tip = disp[["dx[2]", "dy[2]", "dz[2]"]]
 
 plt.plot(ellbow["dz[1]"], label="ellbow")
+plt.plot(ref_disp["Ellbow"]["X"],
+         ref_disp["Ellbow"]["Y"],
+         "--",
+         label="ellbow (Simo et al. 1988)")
 plt.plot(tip["dz[2]"], label="tip")
+plt.plot(ref_disp["Tip"]["X"],
+         ref_disp["Tip"]["Y"],
+         "--",
+         label="tip (Simo et al. 1988)")
 plt.xlim(0, 30)
 plt.ylim(-10, 10)
 plt.legend()
